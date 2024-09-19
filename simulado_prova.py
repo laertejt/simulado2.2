197be5073899e0dfc75f83f41ad5ce320417e98d
laerte <laertejt@gmail.com>
2024-09-19 08:58:54 -0300

    bananinha

--- simulado_prova.py
+++ simulado_prova.py
@@ -3,14 +3,18 @@ tipos_cores = {
     'primaria': ['vermelho', 'azul', 'amarelo'],
     'secundaria': ['verde', 'laranja', 'roxo'],
     }
-
+for key, value in tipos_cores.items():
+    print(value)
 
 # 2 - Faca um for loop na lista e mostre qual tipo ela é
-lst_cores = ['vermelho', 'roxo', 'preto'] 
-
-
-
-
+lst_cores = ['vermelho', 'roxo', 'preto']
+for cor in lst_cores:
+    if cor in tipos_cores['primaria']:
+        print(f"{cor} é primaria")
+    elif cor in tipos_cores['secundaria']:
+        print(f"{cor} é secundaria")
+    else:
+        print(f"{cor} é outro tipo")
 
 # 3 – Verifique o total da compra baseado no preco
 precos = {
@@ -21,8 +25,14 @@ precos = {
     'calça': 145.00,
     'luva': 18.00,
     }
-
 compra = ['luva', 'blusa', 'tenis']
+valor_compra = precos['luva'] + precos['blusa'] + precos['tenis']
+
+total_compra = 0
+for item in compra:
+    preco = precos[item]
+    total_compra = total_compra + preco
+print(f"O total da compra é {total_compra}")
 
 
 # 4 – Faca uma funcao que entre 3 notas e calcule a media final de acordo com o crirterio de nota do ibmec
@@ -31,3 +41,6 @@ compra = ['luva', 'blusa', 'tenis']
 p1 = 8.5
 p2 = 9.0
 ac = 1.0
+from meu_pacote.modulo import calcular_nota
+nota_final = calcular_nota(p1, p2, ac)
+print(f"A nota final foi {nota_final}")
